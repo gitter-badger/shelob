c063d2ff782ccd7317e62050f4746ac88426f25f
breneser <brhneser@gmail.com>
2017-04-24 23:33:23 +0100

    Make repo as pearl package

---
+++ pearl-metadata/config.sh
@@ -0,0 +1,7 @@
+# The following variables can be used:
+# - *PEARL_HOME*       - Pearl location
+# - *PEARL_ROOT*       - Pearl script location
+# - *PEARL_PKGDIR*     - Pearl package location
+# - *PEARL_PKGVARDIR*  - Pearl package var location
+
+export SHELOB_HOME=${PEARL_PKGDIR}
